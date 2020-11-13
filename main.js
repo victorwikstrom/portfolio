6a3803f494b4854bc1674a200aae881d3902a99c
victorwikstrom <victor.wikstrom@icloud.com>
2020-11-13 13:05:50 +0100

    Add content to projects

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-window.addEventListener('load', () => { main() })
+window.addEventListener('load', () => main())
 
 const main = () => {
   setClickEvents()
   setHoverEvents()
-  runIntroText()
+  iterateTextArray(introText)
 }
 
 /**********
@@ -15,6 +15,7 @@ const main = () => {
  const skillElement = document.getElementById('skill-container')
  const bgElement = document.getElementById('background-container')
  const workElement = document.getElementById('work-container')
+ const exitButton = document.getElementById('exit-icon')
  const bgHeadingElement = document.getElementById('background-heading')
  const skillHeadingElement = document.getElementById('skill-heading')
  const workHeadingElement = document.getElementById('work-heading')
@@ -23,25 +24,27 @@ const main = () => {
  const workIconElement = document.getElementById('work-icon')
 
  let containerOpen = false
+ 
+ const introText = [
+   "Hi there!",
+   "Hope you're having a great day so far.",
+   "If you haven't already figured out, my name is Victor.",
+   "I am an aspiring web developer and digital designer, based in Gothenburg.",
+   "I built this site to present myself and some of my recent work...",
+   "... in a simple, yet slightly interactive way.",
+   "The containers below will let you know alot about me...", 
+   "... without even meeting me.", 
+   "Pretty good, right? Considering this bloody pandemic and all...", 
+   "... anyway, enough of me! Or enough of this text that never seem to stop iterating.", 
+   "Have a nice day and make sure to reach out!"
+ ]  
 
 /** Add array of strings and display each string after another */
-const runIntroText = () => {
-  const introText = [
-    "Hi there!",
-    "Hope you're having a great day so far.",
-    "If you haven't already figured out, my name is Victor.",
-    "I am an aspiring web developer and digital designer, based in Gothenburg.",
-    "I built this site to present myself and some of my recent work...",
-    "... in a simple, yet slightly interactive way.",
-    "The containers below include everything you need to know about me...", 
-    "... without even meeting me.", 
-    "Pretty good, right? Considering the pandemic and all...", 
-    "... anyway, enough of me! Or at least this text that never seem to stop iterating.", 
-    "Have a nice day and make sure to reach out!"
-  ]  
+const iterateTextArray = (array) => {
 
   const textElement = document.getElementById('intro-text')
   let textIndex = -1
+  let lastIndex = false
 
   if (textElement.innerText === '') {
     textElement.innerText = 'Hi there!'
@@ -53,30 +56,34 @@ const runIntroText = () => {
   }
 
   function advanceText() {
-    if (textIndex === introText.length - 1) {
-      loadText(introText[textIndex])
+    if (textIndex === array.length - 1) {
+      loadText(array[textIndex])
+      lastIndex = true
     } 
     else {
       textIndex++
-      loadText(introText[textIndex])
+      loadText(array[textIndex])
     }
   }
   function loadText(text) {
     textElement.innerText = text
   }
-  setInterval(advanceText, 2500)
+  if (lastIndex) {
+    clearInterval(advanceText)
+  }
+    setInterval(advanceText, 3500)
 }
 
 /** Set events to clickable elements that require JS */
 const setClickEvents = () => {
   skillElement.addEventListener('click', () => { 
-    openContainer(skillElement, skillContent, skillHeadingElement, skillIconElement)
+    openAndCloseContainer(skillElement, skillContent, skillHeadingElement, skillIconElement)
   })
   bgElement.addEventListener('click', () => {
-    openContainer(bgElement, bgContent, bgHeadingElement, bgIconElement)
+    openAndCloseContainer(bgElement, bgContent, bgHeadingElement, bgIconElement)
   })
   workElement.addEventListener('click', () => {
-    openContainer(workElement, workContent, workHeadingElement, workIconElement)
+    openAndCloseContainer(workElement, workContent, workHeadingElement, workIconElement)
   })
 }
 
@@ -86,7 +93,7 @@ const setHoverEvents = () => {
   hoverContainer(skillElement, skillHeadingElement, skillIconElement)
 }
 
-const openContainer = (element, content, heading, icon) => {
+const openAndCloseContainer = (element, content, heading, icon) => {
   if (element.classList.contains('container-open')) {
     content.classList.remove('content-fadein')
     setTimeout( () => { icon.classList.add('icon-visible') }, 300) 
@@ -101,6 +108,7 @@ const openContainer = (element, content, heading, icon) => {
     setTimeout( () => { content.classList.add('content-fadein') }, 600)
     heading.classList.add('heading-container-open')
     containerOpen = true
+    element.removeEventListener('click', () =>{})
   }
 }
 
